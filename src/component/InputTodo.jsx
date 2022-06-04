be3e38c4d4941ace0c481b672506a061f28e669f
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2022-06-05 04:02:16 +0900

    addを押した後にinput TODOにフォーカスがいくようにした

--- src/component/InputTodo.jsx
+++ src/component/InputTodo.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../css/App.css";
 
 const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current.focus();
+  });
   // console.log(onChange.text);
   // console.log(onChange.deadline);
   return (
     <React.Fragment>
       <div className="input-area wrapper">
         <input
+          ref={ref}
           type="text"
           placeholder="input TODO"
           value={todoText.text}
